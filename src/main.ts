20d40b8001322d09e8413c5a281a918f2141eb5f
NOrbert <62135994+NorbertPointu@users.noreply.github.com>
2024-05-13 17:26:01 +0200

    move display function to utils

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { count } from "./actions/count";
 import { filter } from "./actions/filter";
 import { Countries } from "./types/country-types";
+import { displayRed, displayBold } from "./utils/display";
 import { getActions } from "./utils/getActions";
 import { inspect } from "node:util";
 
-export const main = (data: Countries, actionsInArgs: string[]) => {
+export const main = (givenCoutries: Countries, actionsInArgs: string[]) => {
   const actions = getActions(actionsInArgs);
 
   //Check passing actions
@@ -14,9 +15,9 @@ export const main = (data: Countries, actionsInArgs: string[]) => {
 
   let result: Countries = [];
   if (actions.hasOwnProperty("filter")) {
-    result = filter(data, actions["filter"]);
+    result = filter(givenCoutries, actions["filter"]);
   } else {
-    result = data;
+    result = givenCoutries;
   }
 
   if (actions.hasOwnProperty("count")) {
@@ -31,16 +32,21 @@ const checkPassingActions = (actions: object) => {
     ["filter", "count"].includes(action)
   );
   if (!hasActions) {
-    console.log("\x1B[31mMissing arguments!\x1B[39m");
+    console.log(displayRed("Missing arguments!"));
     console.log(
-      "This app has to be called with arguments \x1B[1m--filter=<value> \x1B[0mor\x1B[1m --count\x1B[0m"
+      `This app has to be called with arguments ${displayBold(
+        "--filter=<value>"
+      )} or ${displayBold("--count")}`
     );
     console.log(
-      "\x1B[1m--filter=pa \x1B[0mwill return people with animal names matching the given filter 'pa'. This search is case sensitive"
+      `${displayBold(
+        "--filter=pa"
+      )} will return people with animal names matching the given filter 'pa'. This search is case sensitive`
     );
     console.log(
-      "\x1B[1m--count\x1B[0m will display the number of items at the end of the name",
-      "\x1B[39m"
+      `${displayBold(
+        "--count"
+      )} will display the number of items at the end of the name`
     );
     return false;
   }
---
+++ src/utils/display.ts
@@ -0,0 +1,2 @@
+export const displayRed = (value: string) => `\x1B[31m${value}\x1B[39m`;
+export const displayBold = (value: string) => `\x1B[1m${value}\x1B[0m`;
